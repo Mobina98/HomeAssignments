c0675e3d380ff737ff6d1ea02ded7f4538bfc602
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2025-09-27 16:05:37 +0300

    Update weapon_type.cpp

--- Assignment5/weapon_type.cpp
+++ Assignment5/weapon_type.cpp
@@ -90,7 +90,7 @@ AWP::AWP(const std::string &name, const unsigned int &damage, const unsigned int
     set_damage(ammo);
 }  // end scope
 
-HeavyUnit::HeavyUnit(const std::string &name, const unsigned int &damage,
+Bazooka::Bazooka(const std::string &name, const unsigned int &damage,
                  const unsigned int &ammo): Gun(name, damage, ammo) {
 /**
  * @brief Function `set_weapon_type`.
